4bf8a68bf16ab6fbace87299bd6305ee22530316
tenntenn <uedatakuya@gmail.com>
2020-09-10 00:37:25 +0900

    Add testdata mod

--- _template/@@.Pkg@@_test.go
+++ _template/@@.Pkg@@_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"@@.ImportPath@@"
+	"github.com/gostaticanalysis/testutil"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 // TestAnalyzer is a test for Analyzer.
 func TestAnalyzer(t *testing.T) {
-	testdata := analysistest.TestData()
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
 	analysistest.Run(t, testdata, @@.Pkg@@.Analyzer, "a")
 }
 
