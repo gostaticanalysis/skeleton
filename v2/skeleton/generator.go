b34bec7be325525c69d3428548dd5ea93f405d5f
tenntenn <uedatakuya@gmail.com>
2021-07-12 22:16:05 +0900

    Execute goimports

--- v2/skeleton/generator.go
+++ v2/skeleton/generator.go
@@ -3,9 +3,11 @@ package skeleton
 import (
 	"bytes"
 	"io/fs"
+	"path/filepath"
 	"text/template"
 
 	"github.com/josharian/txtarfs"
+	"golang.org/x/tools/imports"
 	"golang.org/x/tools/txtar"
 )
 
@@ -18,7 +20,21 @@ func (g *Generator) Run(info *Info) (fs.FS, error) {
 	if err := g.template().Execute(&buf, info); err != nil {
 		return nil, err
 	}
-	return txtarfs.As(txtar.Parse(buf.Bytes())), nil
+	ar := txtar.Parse(buf.Bytes())
+	for i := range ar.Files {
+		if filepath.Ext(ar.Files[i].Name) != ".go" || len(ar.Files[i].Data) == 0 {
+			continue
+		}
+		opt := &imports.Options{
+			FormatOnly: true,
+		}
+		src, err := imports.Process(ar.Files[i].Name, ar.Files[i].Data, opt)
+		if err != nil {
+			return nil, err
+		}
+		ar.Files[i].Data = src
+	}
+	return txtarfs.As(ar), nil
 }
 
 func (g *Generator) template() *template.Template {
